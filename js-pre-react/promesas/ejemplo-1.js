d73a85fa88ee8e7d88e2955be2f2c6b32ecfcaac
Isaac-Fernandez <isaacfdz@outlook.com>
2023-10-31 00:50:34 -0600

    💡 se agrego el codigo completo en los ejemplos

--- js-pre-react/promesas/ejemplo-1.js
+++ js-pre-react/promesas/ejemplo-1.js
@@ -1,15 +1,17 @@
 // EN JSON, LAS PROMESAS SON OBJETOS QUE REPRESENTAN UN VALOR QUE PUEDE ESTAR DISPONIBLE
 // EN UN TIEMPO INDEFINIDO. POR NATURALEZA, SON ESTRUCTURAS ASINCRONAS
 
-// CREAR LA ESTRUCTURA DE  UNA PROMESA
+//1. CREAR LA ESTRUCTURA DE  UNA PROMESA
 const promesa = new Promise(function (resolve, reject) {
     // LAS PROMESAS CONTIENEN DOS METODOS
-    resolve()
-    reject()
+    resolve() //DEVOLVER UN VALOR CUANOD LA PROMESA SE CUMPLE
+    reject() //DEVOLVER UN VALOR O CUALQUIER TIPO DE DATO CUANDO LA PROMESA SE RECHAZA
 })
 
 // 2. CONSUMIR LA PROMESA
 promesa.then(function (response) {
     //codigo
     console.log(response);
+}).catch(function (error) {
+    console.error(error)
 })
--- js-pre-react/promesas/ejemplo-2.js
+++ js-pre-react/promesas/ejemplo-2.js
@@ -6,7 +6,19 @@ let heladeriaAbierta = false
 // 1.CREAR LAS REGLAS DE LA promesa. NOS PUEDE TOCAR HACERLO O NO
 
 const traerHelado = new Promise(function (resolve, reject) {
+    //LOGICA DE LA PROMESA
     if (heladeriaAbierta){
-        resolve("Then")
+        resolve("Ten, te traje un helado porque estaba abierta la heladeria")
+
+    }else{
+        reject("No te traje el helado, porque no estaba abierta la heladeria")
     }
+})
+
+// 2. RECIBIR PROMESAS COMO RESPUESTA
+traerHelado.then(function(response){
+    //codigo...
+    console.log(response);
+}).catch(function(error){
+    console.error("HELADERIA BONITA: " + error);
 })
