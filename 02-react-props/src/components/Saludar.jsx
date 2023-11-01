70c998c6c615764a2010abdb54d7815ed848997c
Isaac-Fernandez <isaacfdz@outlook.com>
2023-10-31 22:20:16 -0600

    ⚡️ Plantilla de landing page base añadida

---
+++ 02-react-props/src/components/Saludar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Saludar = ({nombre, apellido}) => {
+  
+    return (
+    <div>
+        <h1>Hola {nombre} {apellido} </h1>
+    </div>  
+  )
+}
+
+export default Saludar
